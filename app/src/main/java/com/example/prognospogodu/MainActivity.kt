484745b1ff75960132e892f47d524357d8397358
Bodkiss <bbbbodia@gmail.com>
2022-08-18 13:23:41 +0300

    first commit

---
+++ app/src/main/java/com/example/prognospogodu/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.example.prognospogodu
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.core.app.ActivityCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.prognospogodu.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import org.json.JSONObject
+import java.util.*
+import android.provider.Settings
+
+const val API_KEY ="b63ab29574bf484887c81304220906"
+
+class MainActivity : AppCompatActivity() {
+
+    private val permissionId = 2
+    var currentLoc:String= ""
+
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.bGet.setOnClickListener{
+
+            getLocation()
+            getResult(currentLoc)
+        }
+    }
+    private fun getResult(name:String){
+        val url = "https://api.weatherapi.com/v1/current.json"+
+                "?key=$API_KEY&q=$name&aqi=no"
+        val queue = Volley.newRequestQueue(this)
+
+
+
+
+        val sttringRequest = StringRequest(Request.Method.GET,
+            url,
+            {response ->
+                val obj = JSONObject(response)
+                val temp = obj.getJSONObject("current")
+                binding.twTemp.text = temp.getString("temp_c")+"°C"
+                Log.d("MyLog", "Response ${temp.getString("temp_c")}")
+            },
+            { Log.d("MyLog", "Volley Error: $it")}
+        )
+        queue.add(sttringRequest)
+
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: List<Address> =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                        binding.apply {
+                            tvLatitude.text = "Latitude\n${list[0].latitude}"
+                            tvLongitude.text = "Longitude\n${list[0].longitude}"
+                            tvCountryName.text = "Country Name\n${list[0].countryName}"
+                            tvLocality.text = "Locality\n${list[0].locality}"
+                            tvAddress.text = "Address\n${list[0].getAddressLine(0)}"
+                        }
+                        currentLoc = list[0].latitude.toString()+","+list[0].longitude.toString()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+}
